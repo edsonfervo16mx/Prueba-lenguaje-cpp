6f28d5561b74c232b104dcf0f7c7b95952a82d6a
edsonfervo16mx <edson55_@hotmail.com>
2021-01-20 21:08:43 -0600

    Initial commit

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+main (){
+	int a,b,c;
+	printf("Ingrese el primer numero: ");
+	scanf("%d",&a);
+	printf("Ingrese el segundo numero: ");
+	scanf("%d",&b);
+	printf("Ingrese el tercer numero: ");
+	scanf("%d",&c);
+	
+	printf("El primer numero es: %d \n",a);
+	printf("El primer segundo es: %d \n",b);
+	printf("El primer tercer es: %d \n",c);
+}
